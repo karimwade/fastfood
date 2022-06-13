1e1fed31b7c0877c9841a8f373a2b475ff30e2e5
Karim Wade <abdou.karim.info@gmail.com>
2022-06-13 17:05:03 +0000

    Mon premier commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
+class TestController extends Controller
+{
+    //
+
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return \view('test.mytest');
+    }
+
+    public function foo()
+    {
+        if(!Gate::allows('access-admin')){
+            \abort('403');
+        }
+        else {
+            return \view('foo');
+        }
+            
+    }
+}
